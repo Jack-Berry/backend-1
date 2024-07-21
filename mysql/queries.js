67ddfb22808ecf7ea865f2c84058d37aa187a9f9
Your Name <you@example.com>
2024-07-21 18:56:42 +0100

    up to date with lecture

---
+++ mysql/queries.js
@@ -0,0 +1,30 @@
+const addUser = (email, password) => {
+  return `INSERT INTO users (email, password)
+                     VALUES
+                      ("${email}", "${password}");`;
+};
+
+const getId = (email, password) => {
+  return `SELECT id FROM users
+                      WHERE email LIKE "${email}" AND password LIKE "${password}";`;
+};
+
+const addToken = (user_id, token) => {
+  return `INSERT INTO tokens (user_id, token)
+                          VALUE
+                               (${user_id}, "${token}");`;
+};
+
+const getIdFromToken = (token) => {
+  return `SELECT user_id
+                  FROM tokens
+                      WHERE token LIKE "${token}";`;
+};
+
+const setSecret = (id, secret) => {
+  return `UPDATE users
+                  SET secret = "${secret}"
+                      WHERE id = ${id};`;
+};
+
+module.exports = { addUser, getId, addToken, getIdFromToken, setSecret };
